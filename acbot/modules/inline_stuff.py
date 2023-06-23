45dd3de74708d5405a9d67c81c4bab3697b83e3b
AuthorChe | Vadym_Yem <81570039+VadymYem@users.noreply.github.com>
2023-06-23 16:26:03 +0300

    Update inline_stuff.py

--- acbot/modules/inline_stuff.py
+++ acbot/modules/inline_stuff.py
@@ -39,7 +39,7 @@ class InlineStuffMod(loader.Module):
             "✌️ <b>Hello! This is 𝙰𝚞𝚝𝚑𝚘𝚛𝙲𝚑𝚎'𝚜✍️. You can"
             " contact with bot owner via /feedback</b>\n\n<b>🌍 <a"
             ' href="t.me/AuthorChe">𝙰𝚞𝚝𝚑𝚘𝚛𝙲𝚑𝚎✍️</a></b>\n<b>👥 <a'
-            ' href="t.me/ac_ubot">AuthorUB😎</a></b>'
+            ' href="www.instagram.com/Vadym_Yem">Instagram😎</a></b>'
          ),
     }
 
@@ -61,7 +61,7 @@ class InlineStuffMod(loader.Module):
              "✌️ <b>Привіт! Це 𝙰𝚞𝚝𝚑𝚘𝚛𝙲𝚑𝚎'𝚜✍️. Ви можете"
             " зв'язатися з власником боту використавши /feedback</b>\n\n<b>🌍 <a"
             ' href="t.me/AuthorChe">𝙰𝚞𝚝𝚑𝚘𝚛𝙲𝚑𝚎✍️</a></b>\n<b>👥 <a'
-            ' href="https://t.me/ac_ubot">AuthorUB😎</a></b>'
+            ' href="www.instagram.com/Vadym_Yem">Instagram😎</a></b>'
         ),
     }
 
