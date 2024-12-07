e37b46c5712ddbfa96e3eeb3f67ebd1a91b58fe3
AuthorChe | Vadym_Yem <81570039+VadymYem@users.noreply.github.com>
2024-12-07 20:11:37 +0300

    Add files via upload

---
+++ hikka/modules/InlineRandom.py
@@ -0,0 +1,73 @@
+
+
+from random import choice, randint
+
+from .. import loader, utils
+from ..inline.types import InlineQuery
+
+
+@loader.tds
+class InlineRandomMod(loader.Module):
+    """Random tools for your userbot"""
+
+    strings = {"name": "InlineRandom"}
+
+    @loader.inline_everyone
+    async def coin_inline_handler(self, query: InlineQuery) -> dict:
+        """Heads or tails?"""
+
+        r = "🦅 Heads" if randint(0, 1) else "🪙 Tails"
+
+        return {
+            "title": "Toss a coin",
+            "description": "Trust in the God of luck, and he will be by your side!",
+            "message": f"<i>The God of luck tells us...</i> <b>{r}</b>",
+            "thumb": "https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-coin-pirates-justicon-flat-justicon-1.png",
+        }
+
+    @loader.inline_everyone
+    async def random_inline_handler(self, query: InlineQuery) -> dict:
+        """[number] - Send random number less than specified"""
+
+        if not query.args:
+            return
+
+        a = query.args
+
+        if not str(a).isdigit():
+            return
+
+        return {
+            "title": f"Toss random number less or equal to {a}",
+            "description": "Trust in the God of luck, and he will be by your side!",
+            "message": f"<i>The God of luck screams...</i> <b>{randint(1, int(a))}</b>",
+            "thumb": "https://img.icons8.com/external-flaticons-flat-flat-icons/64/000000/external-numbers-auction-house-flaticons-flat-flat-icons.png",
+        }
+
+    @loader.inline_everyone
+    async def choice_inline_handler(self, query: InlineQuery) -> dict:
+        """[args, separated by comma] - Make a choice"""
+
+        if not query.args or not query.args.count(","):
+            return
+
+        a = query.args
+
+        return {
+            "title": "Choose one item from list",
+            "description": "Trust in the God of luck, and he will be by your side!",
+            "message": (
+                "<i>Бог удачі шепоче...</i>"
+                f" <b>{choice(a.split(',')).strip()}</b>"
+            ),
+            "thumb": "https://img.icons8.com/external-filled-outline-geotatah/64/000000/external-choice-customer-satisfaction-filled-outline-filled-outline-geotatah.png",
+        }
+
+    @loader.inline_everyone
+    async def person_inline_handler(self, query: InlineQuery) -> dict:
+        """This person doesn't exist"""
+
+        return {
+            "photo": f"https://thispersondoesnotexist.com/image?id={utils.rand(10)}",
+            "title": "This person doesn't exist",
+        }
---
+++ hikka/modules/update_notifier.py
@@ -0,0 +1,144 @@
+import asyncio
+import contextlib
+
+import git
+
+from .. import loader, utils, version
+from ..inline.types import InlineCall
+from ..types import Message
+
+
+@loader.tds
+class UpdateNotifier(loader.Module):
+    """Tracks latest releases, and notifies you, if update is required"""
+
+    strings = {"name": "UpdateNotifier"}
+
+    def __init__(self):
+        self._notified = None
+        self.config = loader.ModuleConfig(
+            loader.ConfigValue(
+                "disable_notifications",
+                doc=lambda: self.strings("_cfg_doc_disable_notifications"),
+                validator=loader.validators.Boolean(),
+            )
+        )
+
+    def get_changelog(self) -> str:
+        try:
+            repo = git.Repo()
+
+            for remote in repo.remotes:
+                remote.fetch()
+
+            if not (
+                diff := repo.git.log([f"HEAD..origin/{version.branch}", "--oneline"])
+            ):
+                return False
+        except Exception:
+            return False
+
+        res = "\n".join(
+            f"<b>{commit.split()[0]}</b>:"
+            f" <i>{utils.escape_html(' '.join(commit.split()[1:]))}</i>"
+            for commit in diff.splitlines()[:10]
+        )
+
+        if diff.count("\n") >= 10:
+            res += self.strings("more").format(len(diff.splitlines()) - 10)
+
+        return res
+
+    def get_latest(self) -> str:
+        try:
+            return next(
+                git.Repo().iter_commits(f"origin/{version.branch}", max_count=1)
+            ).hexsha
+        except Exception:
+            return ""
+
+    async def client_ready(self):
+        try:
+            git.Repo()
+        except Exception as e:
+            raise loader.LoadError("Can't load due to repo init error") from e
+
+        self._markup = lambda: self.inline.generate_markup(
+            [
+                {"text": self.strings("update"), "data": "hikka/update"},
+                {"text": self.strings("ignore"), "data": "hikka/ignore_upd"},
+            ]
+        )
+
+    @loader.loop(interval=60, autostart=True)
+    async def poller(self):
+        if self.config["disable_notifications"] or not self.get_changelog():
+            return
+
+        self._pending = self.get_latest()
+
+        if (
+            self.get("ignore_permanent", False)
+            and self.get("ignore_permanent") == self._pending
+        ):
+            await asyncio.sleep(60)
+            return
+
+        if self._pending not in {utils.get_git_hash(), self._notified}:
+            m = await self.inline.bot.send_animation(
+                self.tg_id,
+                "https://t.me/hikari_assets/71",
+                caption=self.strings("update_required").format(
+                    utils.get_git_hash()[:6],
+                    '<a href="https://github.com/vadymyem/authorbot/compare/{}...{}">{}</a>'.format(
+                        utils.get_git_hash()[:12],
+                        self.get_latest()[:12],
+                        self.get_latest()[:6],
+                    ),
+                    self.get_changelog(),
+                ),
+                reply_markup=self._markup(),
+            )
+
+            self._notified = self._pending
+            self.set("ignore_permanent", False)
+
+            await self._delete_all_upd_messages()
+
+            self.set("upd_msg", m.message_id)
+
+    async def _delete_all_upd_messages(self):
+        for client in self.allclients:
+            with contextlib.suppress(Exception):
+                await client.loader.inline.bot.delete_message(
+                    client.tg_id,
+                    client.loader.db.get("UpdateNotifier", "upd_msg"),
+                )
+
+    @loader.callback_handler()
+    async def update(self, call: InlineCall):
+        """Process update buttons clicks"""
+        if call.data not in {"hikka/update", "hikka/ignore_upd"}:
+            return
+
+        if call.data == "hikka/ignore_upd":
+            self.set("ignore_permanent", self.get_latest())
+            await call.answer(self.strings("latest_disabled"))
+            return
+
+        await self._delete_all_upd_messages()
+
+        with contextlib.suppress(Exception):
+            await call.delete()
+
+        await self.invoke("update", "-f", peer=self.inline.bot_username)
+
+    @loader.command()
+    async def changelog(self, message: Message):
+        """Shows the changelog of the last major update"""
+        with open('CHANGELOG.md', mode='r', encoding='utf-8') as f:
+            changelog = f.read().split('##')[1].strip()
+        if (await self._client.get_me()).premium:
+            changelog.replace('AuthorBot', '<emoji document_id=5192765204898783881>🌘</emoji><emoji document_id=5195311729663286630>🌘</emoji><emoji document_id=5195045669324201904>🌘</emoji>')
+
+        await utils.answer(message, self.strings('changelog').format(changelog))
