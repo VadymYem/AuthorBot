89ea9ccf0a7148c49547a6f24ce17f0a8a659f1a
AuthorChe | Vadym_Yem <81570039+VadymYem@users.noreply.github.com>
2024-12-07 20:03:52 +0300

    Add files via upload

---
+++ hikka/__init__.py
@@ -0,0 +1,8 @@
+"""Just a placeholder to do relative imports"""
+
+__author__ = "AuthorChe"
+__contact__ = "authorche.pp.ua/cu.html"
+__copyright__ = "Copyright 2022, AuthorChe"
+__license__ = "AGPLv3"
+__maintainer__ = "developer"
+__status__ = "Production"
