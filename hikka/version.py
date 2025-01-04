abfc6d11143b14f9921491c51dfc3644b4ca4d7a
AuthorChe | Vadym_Yem <81570039+VadymYem@users.noreply.github.com>
2025-01-04 23:49:06 +0200

    Виправлення шрифтів, емоджі, логів, перекладів, оновлення каналів з модулями і т.д.

--- hikka/version.py
+++ hikka/version.py
@@ -5,7 +5,7 @@
 # You can redistribute it and/or modify it under the terms of the GNU AGPLv3
 # 🔑 https://www.gnu.org/licenses/agpl-3.0.html
 # I glad to see you here
-__version__ = (1, 9, 2)
+__version__ = (1, 9, 3)
 
 import os
 
